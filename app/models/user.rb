1ca31a1b974763c938de0992843388b56e07ab40
Freigth <cosiconeal@gmail.com>
2022-08-26 16:39:36 +0800

    add login count in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birthdate, presence: true
 end
