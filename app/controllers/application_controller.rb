e1dadff319b2f7b018962bd4adab3bc6595f70d9
Freigth <cosiconeal@gmail.com>
2022-08-26 16:23:03 +0800

    add methods in preferences controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
-    dashboard_path
+    preferences_path
   end
 
   def after_sign_out_path_for(_resource)
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,19 @@
 module PagesHelper
+  def fetch_preferences(params)
+    count = []
+    user_preference = current_user.user_preferences
+    new_preferences = params.select { |key, value| key if value == 'true' }
+    new_preferences.each do |key, _value|
+      next if user_preference.include? key
+
+      user_preference << key
+      count << key
+      current_user.save
+    end
+    count.length
+  end
+
+  def hide_preferences(breed)
+    breed.select { |item| item unless current_user.user_preferences.include? item[:name] }
+  end
 end
