f60b971ea55ed8451bcf4337f6827c5aee3f156e
arielmestrada <estrada.ariel.m@gmail.com>
2022-09-06 19:26:39 +0800

    set migration to one file

--- db/migrate/20220826111809_change_channels_column.rb
+++
@@ -1,6 +0,0 @@
-class ChangeChannelsColumn < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :channels, :group?, :boolean, default: false
-    add_column :channels, :group, :boolean, default: true
-  end
-end
--- db/migrate/20220831122451_add_likes_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddLikesToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :likes, :integer, default: 0
-  end
-end
